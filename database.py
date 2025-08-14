521fcf5d26a908ee7939d89a9b820125032c73e8
kanishaasree <kanishaasree@example.com>
2025-08-14 11:02:01 +0530

    switched account

--- database.py
+++ database.py
@@ -1,24 +1,9 @@
-# import os
-# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-# from sqlalchemy.orm import sessionmaker
-
-# DATABASE_URL = os.getenv("DATABASE_URL", "LOCAL_DB_URL")
-
-# engine = create_async_engine(DATABASE_URL, echo=True, future=True)
-# AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-
-# async def get_session():
-#     async with AsyncSessionLocal() as session:
-#         yield session
-
-      
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 environment = os.getenv("ENVIRONMENT", "local")
 if environment == "prod":
